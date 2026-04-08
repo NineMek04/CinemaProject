4289b69ced0ae32c5c4a6ab06b4576b74f8ba4ae
Tanongsak.pu.intern <tanongsaktsmk2004@gmail.com>
2026-04-08 14:20:53 +0700

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home-component/home-component';
+import { TebberAccount } from './page/account/tebber-account/tebber-account';
 
 export const routes: Routes = [
     {
         path: '',
         component: HomeComponent
+    },
+    {
+        path: 'tebber-account',
+        component: TebberAccount
     }
 ];
