426ff2b4561482a4a7c9d69bb17f7f9bf1bf6342
Tanongsak.pu.intern <tanongsaktsmk2004@gmail.com>
2026-04-08 16:51:30 +0700

    add active topic navbar highlight

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home-component/home-component';
 import { AsideAccount } from './page/components/account/aside-account/tebber-account';
 import { MovieComponent } from './page/components/movie-component/movie-component';
+import { MylistComponent } from './page/components/mylist-component/mylist-component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: 'home',
         component: HomeComponent,
     },
     {
         path: 'tebber-account',
         component: AsideAccount
     },
-    {path: 'movies', component: MovieComponent}
-    ,
-    { path: '**', redirectTo: '' }
+    {path: 'movies', component: MovieComponent},
+    {path: 'mylist', component: MylistComponent},
+    { path: '**', redirectTo: 'home' }
 ];
---
+++ src/app/page/components/mylist-component/mylist-component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MylistComponent } from './mylist-component';
+
+describe('MylistComponent', () => {
+  let component: MylistComponent;
+  let fixture: ComponentFixture<MylistComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [MylistComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(MylistComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
