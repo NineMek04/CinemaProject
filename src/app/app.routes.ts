d346bd086d4736ba5fe2c6c4386fc70aeee719ab
Tanongsak.pu.intern <tanongsaktsmk2004@gmail.com>
2026-04-08 16:15:15 +0700

    ่kkkk

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './page/home/home-component/home-component';
 import { AsideAccount } from './page/components/account/aside-account/tebber-account';
+import { MovieComponent } from './page/components/movie-component/movie-component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
     },
     {
         path: 'tebber-account',
         component: AsideAccount
-    }
+    },
+    {path: 'movies', component: MovieComponent}
+    ,
+    { path: '**', redirectTo: '' }
 ];
