b9b20766514c32d399c9079565f2239b02265324
Tanongsak.pu.intern <tanongsaktsmk2004@gmail.com>
2026-04-09 17:03:48 +0700

    feat: enhance app configuration with smooth scrolling and toast border styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter , withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +7,13 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    // provideRouter(
+    //   routes,
+    //   // 🌟 2. ใส่โค้ดนี้เข้าไป เพื่อบังคับให้เลื่อนไปบนสุด (top) เสมอเวลาเปลี่ยนหน้า
+    //   withInMemoryScrolling({ 
+    //     scrollPositionRestoration: 'disabled',
+    //   })
+    // )
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, signal ,PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './page/home/components/navbar-component/navbar-component';
 import { ToastComponent } from './shared/components/toast-component/toast-component';
+import { filter } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
@@ -9,6 +12,27 @@ import { ToastComponent } from './shared/components/toast-component/toast-compon
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('Cinema');
+
+
+  private router = inject(Router);
+  private platformId = inject(PLATFORM_ID);
+
+ ngOnInit() {
+  
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe(() => {
+      
+      if (isPlatformBrowser(this.platformId)) {
+        // โค้ดข้างในนี้จะทำงานก็ต่อเมื่ออยู่บนเว็บบราวเซอร์เท่านั้น (Server จะข้ามไปเลย)
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth'
+        });
+      }
+
+    });
+  }
 }
